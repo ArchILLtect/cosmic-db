202e9fd72e9aa94e6a66f4698297554cda485f78
ArchILLtect <nickhansonsr@gmail.com>
2025-05-08 22:53:03 -0500

    Fix broken import in index.php

--- index.php
+++ index.php
@@ -44,72 +44,144 @@
                         <?= $_SESSION['user_access_privileges'] ?></span>-level access.
                 </h3>
                 <?php endif; ?>
-                    <?php
-                        require_once('dbconnection.php');
-                        require_once('fileconstants.php');
-                        require_once('species/speciesfileconstants.php');
-                        require_once('characters/characterfileconstants.php');
+                <?php
+                    require_once('dbconnection.php');
+                    require_once('fileconstants.php');
+                    require_once('characters/characterfileconstants.php');
 
-                        $dbc = mysqli_connect(  DB_HOST,
-                                                DB_USER,
-                                                DB_PASSWORD,
-                                                DB_NAME,
-                                                DB_PORT)
-                                or trigger_error('Error connecting to MySQL server for'
-                                .  DB_NAME, E_USER_ERROR);
+                    $dbc = mysqli_connect(  DB_HOST,
+                                            DB_USER,
+                                            DB_PASSWORD,
+                                            DB_NAME,
+                                            DB_PORT)
+                            or trigger_error('Error connecting to MySQL server for'
+                            .  DB_NAME, E_USER_ERROR);
 
-                        $query = "SELECT species_id, name, image_file FROM species ORDER BY name";
+                    $query = "SELECT character_id, name, image_file FROM characters ORDER BY name";
 
-                        $result = mysqli_query($dbc, $query)
-                                or trigger_error('Error querying database species', 
-                                E_USER_ERROR);
+                    $result = mysqli_query($dbc, $query)
+                            or trigger_error('Error querying database characters', 
+                            E_USER_ERROR);
 
-                        if (mysqli_num_rows($result) > 0):
-                    ?>
-                        <table class="table table-striped table-hover">
-                            <thead>
-                            <tr>
-                                <th scope="col">Species Name</th>
-                                <th scope="col"></th>
-                                <th scope="col"></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                    while($row = mysqli_fetch_assoc($result))
+                    if (mysqli_num_rows($result) > 0):
+                ?>
+                    <h2 class="text-center">Characters</h2>
+                    <table class="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Character Name</th>
+                            <th scope="col"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                while($row = mysqli_fetch_assoc($result))
+                                {
+                                    $character_image_file = $row['image_file'];
+                                    
+                                    if (empty($character_image_file))
                                     {
-                                        $species_image_file = $row['image_file'];
-                                        
-                                        if (empty($species_image_file))
-                                        {
-                                            $species_image_file = CDB_UPLOAD_WEB_PATH
-                                                    . CDB_DEFAULT_SPECIES_FILENAME;
-                                        }
-                                        
-                                        echo "<tr><td><img src=" . htmlspecialchars($species_image_file)
-                                                . " class='img-thumbnail'"
-                                                . "style='max-height: 75px;' alt='Species Image'"
-                                                . "</td><td class='align-middle'>"
-                                                . "<a class='nav-link' href='species/speciesdetails.php?id="
-                                                . $row['species_id'] . "'>" . htmlspecialchars($row['name'])
-                                                . "</a></td><td>";
-                                        if (isset($_SESSION['user_access_privileges']) &&
-                                                $_SESSION['user_access_privileges'] == 'admin') {
-                                            echo "<a class='nav-link'"
-                                                    . "href='species/removespecies.php?id_to_delete=" . $row['species_id']
-                                                    . "'><i class='fas fa-trash-alt'></i></a></td></tr>";
-                                        }
+                                        /* TODO: Return to this code when the fileutil is fixed
+                                        $species_image_file = CDB_UPLOAD_WEB_PATH
+                                                . CDB_DEFAULT_SPECIES_FILENAME;
+                                        */
+                                        $species_image_file = CDB_DEFAULT_SPECIES_FILENAME;
                                     }
-                                ?>
-                            </tbody>
-                        </table>        
-                    <?php
-                        else:
-                    ?>
-                            <h3>No Species Found</h3>
-                    <?php
-                        endif;           
-                    ?>
+                                    // TODO: Fix fileutil to add 'images/' to the path, and remove it from here
+                                    echo "<tr><td><img src=" . "images/" . htmlspecialchars($character_image_file)
+                                            . " class='img-thumbnail'"
+                                            . "style='max-height: 75px;' alt='Character Image'"
+                                            . "</td><td class='align-middle'>"
+                                            . "<a class='nav-link' href='characters/characterdetails.php?id="
+                                            . $row['character_id'] . "'>" . htmlspecialchars($row['name'])
+                                            . "</a></td><td>";
+                                    if (isset($_SESSION['user_access_privileges']) &&
+                                            $_SESSION['user_access_privileges'] == 'admin') {
+                                        echo "<a class='nav-link'"
+                                                . "href='characters/removecharacter.php?id_to_delete=" . $row['character_id']
+                                                . "'><i class='fas fa-trash-alt'></i></a></td></tr>";
+                                    }
+                                }
+                            ?>
+                        </tbody>
+                    </table>        
+                <?php
+                    else:
+                ?>
+                        <h3>No Characters Found</h3>
+                <?php
+                    endif;           
+                ?>
+                <?php
+                    require_once('dbconnection.php');
+                    require_once('fileconstants.php');
+                    require_once('species/speciesfileconstants.php');
+
+                    $dbc = mysqli_connect(  DB_HOST,
+                                            DB_USER,
+                                            DB_PASSWORD,
+                                            DB_NAME,
+                                            DB_PORT)
+                            or trigger_error('Error connecting to MySQL server for'
+                            .  DB_NAME, E_USER_ERROR);
+
+                    $query = "SELECT species_id, name, image_file FROM species ORDER BY name";
+
+                    $result = mysqli_query($dbc, $query)
+                            or trigger_error('Error querying database species', 
+                            E_USER_ERROR);
+
+                    if (mysqli_num_rows($result) > 0):
+                ?>
+                    <h2 class="text-center">Species</h2>
+                    <table class="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Species Name</th>
+                            <th scope="col"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                while($row = mysqli_fetch_assoc($result))
+                                {
+                                    $species_image_file = $row['image_file'];
+                                    
+                                    if (empty($species_image_file))
+                                    {
+                                        /* TODO: Return to this code when the fileutil is fixed
+                                        $species_image_file = CDB_UPLOAD_WEB_PATH
+                                                . CDB_DEFAULT_SPECIES_FILENAME;
+                                        */
+                                        $species_image_file = CDB_DEFAULT_SPECIES_FILENAME;
+                                    }
+                                    // TODO: Fix fileutil to add 'images/' to the path, and remove it from here
+                                    echo "<tr><td><img src=" . "images/" . htmlspecialchars($species_image_file)
+                                            . " class='img-thumbnail'"
+                                            . "style='max-height: 75px;' alt='Species Image'"
+                                            . "</td><td class='align-middle'>"
+                                            . "<a class='nav-link' href='species/speciesdetails.php?id="
+                                            . $row['species_id'] . "'>" . htmlspecialchars($row['name'])
+                                            . "</a></td><td>";
+                                    if (isset($_SESSION['user_access_privileges']) &&
+                                            $_SESSION['user_access_privileges'] == 'admin') {
+                                        echo "<a class='nav-link'"
+                                                . "href='species/removespecies.php?id_to_delete=" . $row['species_id']
+                                                . "'><i class='fas fa-trash-alt'></i></a></td></tr>";
+                                    }
+                                }
+                            ?>
+                        </tbody>
+                    </table>        
+                <?php
+                    else:
+                ?>
+                        <h3>No Species Found</h3>
+                <?php
+                    endif;           
+                ?>
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
