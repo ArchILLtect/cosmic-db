0b21bc6d5002e9a953b16d3ae5cb1d43c34c0c4e
ArchILLtect <nickhansonsr@gmail.com>
2025-05-08 20:26:03 -0500

    Update fileconstants to point to the correct file paths for images

--- fileconstants.php
+++ fileconstants.php
@@ -4,5 +4,6 @@
 -->
 <?php
 // Species Listing File Constants
-    define('CDB_UPLOAD_PATH', 'images/');
+    define('CDB_UPLOAD_PATH', $_SERVER['DOCUMENT_ROOT'] . '/cosmic-db/images/');
+    define('CDB_UPLOAD_WEB_PATH', '/cosmic-db/images/');
     define('CDB_MAX_FILE_SIZE', 524288);
