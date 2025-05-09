5f09a01667c336461bca37a2dccb92de2a3ab633
ArchILLtect <nickhansonsr@gmail.com>
2025-05-08 19:25:18 -0500

    Add general fileconstants

---
+++ fileconstants.php
@@ -0,0 +1,8 @@
+<!--    Author: Nick Hanson
+	      Version: 0.3
+	      Date: 4/20/25
+-->
+<?php
+// Species Listing File Constants
+    define('CDB_UPLOAD_PATH', 'images/');
+    define('CDB_MAX_FILE_SIZE', 524288);
