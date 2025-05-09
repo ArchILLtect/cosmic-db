b761a6197c0e3acd802318af8b8549b170679553
ArchILLtect <nickhansonsr@gmail.com>
2025-05-08 19:07:23 -0500

    Update pagetitles.php to account for new character module

--- pagetitles.php
+++ pagetitles.php
@@ -4,11 +4,29 @@
 -->
 <?php
     // Page Titles
+    // Purpose:         Defines the page titles for the Cosmic Database
+    //                  web application.
+    // Description:     Defines the page titles for the Cosmic Database
+    //                  web application. The page titles are used to
+    //                  set the title of each page in the web application.
+    //                  The page titles are defined as constants and
+    //                  are used in the header of each page.
+
+    // Main Pages
     define('CDB_HOME_PAGE', 'Cosmic Database');
     define('CDB_DETAILS_PAGE', 'Details - Cosmic Database');
     define('CDB_SIGNUP_PAGE', 'Sign Up - Cosmic Database');
     define('CDB_LOGIN_PAGE', 'Login - Cosmic Database');
     define('CDB_UNAUTHORIZED_ACCESS_PAGE', 'Unauthorized Access - Cosmic Database');
+
+    // Species Pages
+    //define('CDB_SPECIES_PAGE', 'Species - Cosmic Database');
     define('CDB_ADD_SPECIES_PAGE', 'Add Species - Cosmic Database');
     define('CDB_EDIT_SPECIES_PAGE', 'Edit Species - Cosmic Database');
     define('CDB_REMOVE_SPECIES_PAGE', 'Remove Species - Cosmic Database');
+
+    // Character Pages
+    //define('CDB_SPECIES_PAGE', 'Species - Cosmic Database');
+    define('CDB_ADD_CHARACTER_PAGE', 'Add Character - Cosmic Database');
+    define('CDB_EDIT_CHARACTER_PAGE', 'Edit Character - Cosmic Database');
+    define('CDB_REMOVE_CHARACTER_PAGE', 'Remove Character - Cosmic Database');
