8a919be90c29a78d820bd690739f3a4a358facc8
ArchILLtect <nickhansonsr@gmail.com>
2025-05-10 01:50:35 -0500

    Add footer page/functionality

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- Author: Nick Hanson
+     Version: 0.1
+     Date: 4/20/25
+-->
+<footer class="footer text-white text-center py-2 mt-auto fixed-bottom" style="background-color: #569f32">
+    <div class="container">
+        <small>&copy; <?= date('Y') ?> Cosmic Database &nbsp;|&nbsp; 
+            <a href="/cosmic-db/logout.php" class="text-white font-weight-bold">Logout</a>
+        </small>
+    </div>
+</footer>
