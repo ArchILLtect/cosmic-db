fc0196462ea92f1ddc6efece7d347487c73bd4de
ArchILLtect <nickhansonsr@gmail.com>
2025-05-14 13:57:08 -0500

    Remove obselete port parameter from database connection definition

--- dbconnection.php
+++ dbconnection.php
@@ -5,16 +5,14 @@
 <?php
     // Database connection constants
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'cosmic-db');
-    define('DB_PORT', 3308);
+    define('DB_USER', 'bdukyxmy_nick');
+    define('DB_PASSWORD', 'a5D5uiAJd28YsLF');
+    define('DB_NAME', 'bdukyxmy_cosmicdb');
 
     $dbc = mysqli_connect(  DB_HOST,
                             DB_USER,
                             DB_PASSWORD,
-                            DB_NAME,
-                            DB_PORT)
+                            DB_NAME)
                         or trigger_error('Error connecting to MySQL server for'
                         .  DB_NAME, E_USER_ERROR)
     ;
